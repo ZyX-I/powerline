ae101e9a0d0fc66ed6eb1de8887665dc6ef54d1d
ZyX <kp-pav@yandex.ru>
2014-11-08 14:04:50 +0300

    Stash away attempt to use MatchWindow/@@buffer to detect Command-T

--- powerline/matchers/vim/plugin/commandt.py
+++ powerline/matchers/vim/plugin/commandt.py
@@ -1,11 +1,75 @@
 # vim:fileencoding=utf-8:noet
 from __future__ import (unicode_literals, division, absolute_import, print_function)
 
-import os
+try:
+	import vim
+except ImportError:
+	vim = object()
 
-from powerline.bindings.vim import buffer_name
+from powerline.bindings.vim import create_ruby_dpowerline
+
+
+initialized = False
+
+
+def initialize():
+	global initialized
+	if initialized:
+		return
+	initialized = True
+	create_ruby_dpowerline()
+	vim.command((
+		# When using :execute (vim.command uses the same code) one should not 
+		# use << EOF.
+		'''
+		ruby
+		def $powerline.initialize
+			if @initialized or $command_t == nil
+				return
+			end
+			@initialized = true
+			if (not ($command_t.respond_to? 'active_finder'))
+				def $command_t.active_finder
+					@active_finder.class.name
+				end
+			end
+			if (not ($command_t.respond_to? 'path'))
+				def $command_t.path
+					@path
+				end
+			end
+			if (not ($command_t.respond_to? 'is_own_buffer'))
+				def $command_t.is_own_buffer buffer_number
+					if @match_window
+						if (not (@match_window.respond_to? 'buffer_number'))
+							def @match_window.buffer_number
+								@@buffer and @@buffer.number
+							end
+						end
+						return buffer_number == @match_window.buffer_number
+					else
+						return false
+					end
+				end
+			end
+		end
+		def $powerline.commandt_set_active_finder
+			$powerline.initialize
+			::VIM::command "let g:powerline_commandt_reply = '#{$command_t.active_finder}'"
+		end
+		def $powerline.commandt_set_path
+			$powerline.initialize
+			::VIM::command "let g:powerline_commandt_reply = '#{$command_t.path.gsub(/'/, "''")}'"
+		end
+		def $powerline.commandt_set_own_buffer buffer_number
+			$powerline.initialize
+			::VIM::command "let g:powerline_commandt_reply = #{($command_t != nil and $command_t.is_own_buffer buffer_number) and 1 or 0}"
+		end
+		'''
+	))
 
 
 def commandt(matcher_info):
-	name = buffer_name(matcher_info)
-	return name and os.path.basename(name) == b'GoToFile'
+	initialize()
+	vim.command('ruby $powerline.commandt_set_own_buffer ' + str(matcher_info['bufnr']))
+	return int(vim.eval('g:powerline_commandt_reply'))
--- powerline/segments/vim/plugin/commandt.py
+++ powerline/segments/vim/plugin/commandt.py
@@ -6,42 +6,6 @@ try:
 except ImportError:
 	vim = object()
 
-from powerline.bindings.vim import create_ruby_dpowerline
-
-
-def initialize():
-	global initialized
-	if initialized:
-		return
-	initialized = True
-	create_ruby_dpowerline()
-	vim.command((
-		# When using :execute (vim.command uses the same code) one should not 
-		# use << EOF.
-		'''
-		ruby
-		if (not ($command_t.respond_to? 'active_finder'))
-			def $command_t.active_finder
-				@active_finder.class.name
-			end
-		end
-		if (not ($command_t.respond_to? 'path'))
-			def $command_t.path
-				@path
-			end
-		end
-		def $powerline.commandt_set_active_finder
-			::VIM::command "let g:powerline_commandt_reply = '#{$command_t.active_finder}'"
-		end
-		def $powerline.commandt_set_path
-			::VIM::command "let g:powerline_commandt_reply = '#{$command_t.path.gsub(/'/, "''")}'"
-		end
-		'''
-	))
-
-
-initialized = False
-
 
 def finder(pl):
 	'''Display Command-T finder name
@@ -49,7 +13,6 @@ def finder(pl):
 	Requires $command_t.active_finder and .path methods (code above may 
 	monkey-patch $command_t to add them).
 	'''
-	initialize()
 	vim.command('ruby $powerline.commandt_set_active_finder')
 	return [{
 		'highlight_group': ['commandt:finder'],
@@ -66,7 +29,6 @@ FINDERS_WITHOUT_PATH = set((
 
 
 def path(pl):
-	initialize()
 	vim.command('ruby $powerline.commandt_set_active_finder')
 	finder = vim.eval('g:powerline_commandt_reply')
 	if finder in FINDERS_WITHOUT_PATH:
